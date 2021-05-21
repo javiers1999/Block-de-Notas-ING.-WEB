ce0cbc82cfb371afe972ef61d33a392ea462675c
javiers1999 <56318901+javiers1999@users.noreply.github.com>
2021-05-21 17:41:14 -0400

    casi terminado

--- src/app/componente/componente.component.ts
+++ src/app/componente/componente.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DatosService } from '../datos.service';
+import { Interfaz } from '../interfaz'
 
 
 @Component({
--- src/app/interfaz.ts
+++ src/app/interfaz.ts
@@ -1,2 +1,5 @@
 export interface Interfaz {
+    titulo:String,
+    estado:String,
+    descripcion:String
 }
